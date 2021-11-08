07fbd852b2a0c9f354bc9110fbee07c4078fee11
Alexandr <86725678+AlexandrMrt@users.noreply.github.com>
2021-11-08 14:14:32 +0300

    Lesson 5

---
+++ Lesson 5.cpp
@@ -0,0 +1,79 @@
+﻿
+#include <iostream>
+
+//Task 1
+void FillArr(const int SIZE, double ARR[])
+{
+	for (int i = 0; i < SIZE; i++)
+	{
+		ARR[i] = (rand() % 101) / 10.0;
+	}
+}
+
+void PrintArr(const int SIZE, double ARR[])
+{
+	for (int i = 0; i < SIZE; i++)
+	{
+		std::cout << ARR[i] << " ";
+	}
+	std::cout << std::endl;
+}
+
+//Task 2
+void InvertArr(const int SIZE, int ARR[])
+{
+	std::cout << "before invert: ";
+	for (int i = 0; i < SIZE; ++i)
+	{
+		std::cout << ARR[i] << " ";
+		ARR[i] = !ARR[i];
+	}
+	std::cout << std::endl;
+	std::cout << "after invert:  ";
+	for (int j = 0; j < SIZE; ++j)
+	{
+		std::cout << ARR[j] << " ";
+	}
+	std::cout << std::endl;
+}
+
+
+//Task 3
+void Arr(const int SIZE, int ARR[])
+{
+	for (int i = 0, j = 1; i < SIZE; j += 3, ++i) 
+	{
+		std::cout << j << " ";
+	}
+	std::cout << std::endl;
+}
+
+
+
+
+
+int main()
+{
+	const int SIZE = 8;
+	
+	//Task 1
+	{
+		double ARR[SIZE];
+		FillArr(SIZE, ARR);
+		PrintArr(SIZE, ARR);
+	}
+	
+	//Task 2
+	{
+		int ARR[SIZE];
+		for (int i = 0; i < SIZE; i++)
+		{
+			ARR[i] = rand() % 2;
+		}
+		InvertArr(SIZE, ARR);
+	}
+	
+	//Task 3
+	int ARR[SIZE];
+	Arr(SIZE, ARR);
+}
